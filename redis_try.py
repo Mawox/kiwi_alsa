aa880e0935570a7a90f4dc6105feb80feca0a503
mawox <maw00x@gmail.com>
2018-09-08 14:55:39 +0200

    inintial working commit

--- redis_try.py
+++
@@ -1,9 +0,0 @@
-from redis import StrictRedis
-
-redis_config = {"host":"35.198.72.72", "port":3389}
-
-redis = StrictRedis(socket_connect_timeout=3, **redis_config)
-
-redis.set("x", 5)
-
-redis.get("x")
